34d453f2f23e280ad18134c01a71ecfb3dc0b85f
Christopher <chriskindl@gmail.com>
2016-05-08 14:07:42 +0200

    Saver: Pruefung auf Fehler beim Schreiben hinzugefuegt

--- Saver.cpp
+++ Saver.cpp
@@ -21,6 +21,7 @@
 bool Saver::autosave_enabled_ = false;
 std::string Saver::autosave_filename_ = "";
 
+
 //------------------------------------------------------------------------------
 Saver::Saver(const std::string filename) : Filehandler(filename)
 {
@@ -43,30 +44,25 @@ Command::Status Saver::save(const Game& current_game)
   {
     return Command::FILE_NOT_WRITTEN_;
   }
-  else
+  
+  file << current_game.getFinishedTurns() << "\n";
+  file << current_game.getMaxTurns() << "\n";
+  
+  const std::vector<std::vector<Field*> >* board = current_game.getBoard();
+  
+  for (unsigned int y = 0; y < board->size(); y++)
   {
-    
-    file << current_game.getFinishedTurns() << "\n";
-    file << current_game.getMaxTurns() << "\n";
-    
-    const std::vector<std::vector<Field*> >* board = current_game.getBoard();
-    
-    for (unsigned int y = 0; y < board->size(); y++)
+    for (unsigned int x = 0; x < board->at(y).size(); x++)
     {
-      for (unsigned int x = 0; x < board->at(y).size(); x++)
-      {
-        
-        file << board->at(y).at(x)->getFieldSymbol(Field::FOR_SAVING);
-      }
-
-      file << "\n";
+      file << board->at(y).at(x)->getFieldSymbol(Field::FOR_SAVING);
     }
     
-    
-    file.close();
+    file << "\n";
   }
-   
-  return Command::OK_;
+  
+  file.close();
+  
+  return (file.fail()) ? Command::FILE_NOT_WRITTEN_ : Command::OK_;
 
 }
   
